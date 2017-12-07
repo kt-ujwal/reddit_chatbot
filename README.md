6a4b93e8456ba3f5df3e0a26c5390e9755502f9a
koo <zhengqun.koo@gmail.com>
2017-12-07 21:37:12 +0800

    *.to and *.from written to nmt-chatbot/new_data

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# Reddit Chatbot.
+# Reddit Chatbot
 Following [sentdex's tutorial][playlist] on neural machine translation, with some tweaks.
 
 ## Usage
-Edit parameters in `chatbot_databese.py`.
-
-To get SQL database, run: `python3 chatbot_database.py`.
-
-To get `train.from` and `train.to`, run: `python3 training_data.py`.
+1. `git clone --recursive https://github.com/zhengqunkoo/reddit_chatbot.git`
+2. Edit parameters in `chatbot_databese.py` and `training_data.py`.
+3. To get SQL database, run: `python3 chatbot_database.py`.
+4. To get `train.from` and `train.to`, run: `python3 training_data.py`.
+5. Follow [nmt-chatbot instructions][nmt-chatbot]
 
 ## Notes
-To save time, `training_data.py` processes all data at once in RAM.
+For one month of reddit comments (53gb JSON), `chatbot_database.py` takes about 30 minutes, to produce a 1.6gb database.
+To save time, `training_data.py` processes about 1 million rows of data in 10 seconds with about 512mb of RAM.
 
 [playlist]: https://www.youtube.com/playlist?list=PLQVvvaa0QuDdc2k5dwtDTyT9aCja0on8j
+[nmt-chatbot]: https://github.com/daniel-kukiela/nmt-chatbot
--- training_data.py
+++ training_data.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 # Parameters
 master_folder = "/media/bigdata/" # Folder containing data / SQL.
+train_data_folder = "nmt-chatbot/new_data/" # Folder for *.to and *.from.
 table = "parent_reply"            # Name of SQL table.
 time_year = "2017"                # Year of data.
 time_month = "10"                 # Month of data.
@@ -21,11 +22,11 @@ for timeframe in timeframes:
     df = pd.read_sql("SELECT * FROM {} WHERE parent NOT NULL ORDER BY created_utc ASC".format(table), connection)
     df_length = len(df)
     split_index = int(train_test_split * df_length)
-    with open("train.from", "w", encoding="utf8") as ff, open("train.to", "w", encoding="utf8") as tf:
+    with open(train_data_folder + "train.from", "w", encoding="utf8") as ff, open(train_data_folder + "train.to", "w", encoding="utf8") as tf:
         for index in range(split_index):
             ff.write(df["parent"].values[index]+'\n')
             tf.write(df["body"].values[index]+'\n')
-    with open("test.to", "w", encoding="utf8") as ff, open("test.from", "w", encoding="utf8") as tf:
+    with open(train_data_folder + "test.to", "w", encoding="utf8") as ff, open(train_data_folder + "test.from", "w", encoding="utf8") as tf:
         for index in range(split_index, df_length):
             ff.write(df["parent"].values[index]+'\n')
             tf.write(df["body"].values[index]+'\n')
